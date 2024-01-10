3aa982b2b49924e85656dbf8c1b6e4aa9e308c30
shivamkumarsingh28 <110661272+shivamkumarsingh28@users.noreply.github.com>
2024-01-10 08:05:29 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -37,7 +37,6 @@ app.post('/', async (req, res) => {
 
     res.status(200).send({
       bot: response.data.choices[0].text
-      console.log(response.data.choices[0].text)
     });
 
   } catch (error) {
